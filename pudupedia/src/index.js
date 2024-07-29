8eab7e31aabf6b608337116b23a09979a867a5f1
Biank123 <biancadepetris1998@gmail.com>
2024-07-29 19:37:37 -0400

    update title image

--- pudupedia/src/index.js
+++ pudupedia/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import reportWebVitals from './reportWebVitals';
+import LandingPage from './components/LandingPage';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <LandingPage/>
   </React.StrictMode>
 );
 
