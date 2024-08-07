91b2131eb15d5e6fda374f07c4fbc162761c7eaf
ghiraghira <guzmanorellanap@gmail.com>
2024-08-07 17:59:26 -0400

    ingreso/registro actualizado

--- pudupedia/src/components/signlog/SignLog.js
+++ pudupedia/src/components/signlog/SignLog.js
@@ -1,58 +1,140 @@
-import React from "react";
-import "./SignLog.css";
-import "./SignLogInt.js"
+import React, { useState } from 'react';
+import './SignLog.css'; 
+
 const SignAndLog = () => {
-	const signUpButton = document.getElementById('signUp');
-	const signInButton = document.getElementById('signIn');
-	const container = document.getElementById('container');
-	
-	signUpButton.addEventListener('click', () => {
-		container.classList.add("right-panel-active");
-	});
-	
-	signInButton.addEventListener('click', () => {
-		container.classList.remove("right-panel-active");
-	});
-	
-  return ( 
-    <div className="bothContainer">
-            <div class="container" id="container">
-      	<div class="form-container sign-up-container">
-      		<form action="#">
-      			<h1>Crea una cuenta</h1>
-      			<span></span>
-      			<input type="text" placeholder="Nombre de Usuario" />
-      			<input type="email" placeholder="Email" />
-      			<input type="password" placeholder="Contraseña" />
-      			<input type="password" placeholder="Confirmar Contraseña" />
-      			<button className="signUp">Crear Cuenta</button>
-      		</form>
-      	</div>
-      	<div class="form-container sign-in-container">
-      		<form action="#">
-      			<h1>Ingresa</h1>
-      			<span></span>
-      			<input type="email" placeholder="Email" />
-      			<input type="password" placeholder="Contraseña" />
-      			<a href="#">¿Olvidaste tu Contraseña?</a>
-      			<button className="signUp">Ingresar</button>
-      		</form>
-      	</div>
-      	<div class="overlay-container">
-      		<div class="overlay">
-      			<div class="overlay-panel overlay-left">
-      				<h1 className="question">¿Ya tienes una cuenta?</h1>
-      				<button class="ghost" id="signIn">Ingresa</button>
-      			</div>
-      			<div class="overlay-panel overlay-right">
-      				<h1 className="question">¿Aún no te registras?</h1>
-      				<button class="ghost" id="signUp">Crea una Cuenta</button>
-      			</div>
-      		</div>
-      	</div>
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsSignUp(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUp(false);
+  };
+
+  const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirm, setConfirm] = useState("");
+
+
+	const handleSubmit = (e) => {
+			e.preventDefault();
+			console.log(
+					username,
+					email,
+					password,
+					confirm,
+			);
+	};
+
+  return (
+    <div className={`container ${isSignUp ? 'rightPanelActive' : ''}`}>
+      <div className="formContainer signUpContainer">
+        <form action="#" method='get'>
+          <h1>Crea una Cuenta</h1>
+          <span></span>
+          <input 
+						 type="text"                        
+						 name="username"
+             id="username"
+             value={username}
+             onChange={(e) =>
+              setUsername(e.target.value)
+             }
+						 placeholder="Nombre de Usuario" 
+						 required
+						/>
+          <input 
+						 type="email" 
+             name="email"
+             id="email"
+             value={email}
+             onChange={(e) =>
+              setEmail(e.target.value)
+             }
+						 placeholder="Email" 
+						 required
+					/>
+      		<input 
+						 type="password" 
+						 name="password"
+						 id="password"
+						 value={password}
+						 onChange={(e) =>
+							setPassword(e.target.value)
+						 }
+						 placeholder="Contraseña"
+						 required
+					/>
+      		<input 
+						 type="password" 
+						 name="confirmPassword"
+						 id="confirmPassword"
+						 value={confirm}
+						 onChange={(e) =>
+							setConfirm(e.target.value)
+						 }
+						 placeholder="Confirmar Contraseña"
+						 required
+					/>
+      		<button 
+						 className="signUp"
+						 type="submit"
+						 value="submit"
+						 onClick={(e) => handleSubmit(e)}
+					>Crear Cuenta</button>
+        </form>
+      </div>
+      <div className="formContainer signInContainer">
+        <form action="#" method='get'>
+          <h1>Ingresa</h1>
+          <span></span>
+          <input 
+						 type="email" 
+             name="email"
+             id="email"
+             value={email}
+             onChange={(e) =>
+              setEmail(e.target.value)
+             }
+						 placeholder="Email" 
+						 required
+					/>
+      		<input 
+						 type="password" 
+						 name="password"
+						 id="password"
+						 value={password}
+						 onChange={(e) =>
+							setPassword(e.target.value)
+						 }
+						 placeholder="Contraseña"
+						 required
+					/>
+      		<a href="#">¿Olvidaste tu Contraseña?</a>
+      		<button 
+						 className="signIn" 
+						 type="submit"
+						 value="submit"
+						 onClick={(e) => handleSubmit(e)}
+					>Ingresar</button>
+        </form>
+      </div>
+      <div className="overlayContainer">
+        <div className="overlay">
+          <div className="overlayPanel overlayLeft">
+            <h1 className='question'>¿Ya tienes una cuenta?</h1>
+            <button className="ghost" onClick={handleSignInClick}>Ingresa</button>
+          </div>
+          <div className="overlayPanel overlayRight">
+            <h1 className='question'>¿Aún no te registras?</h1>
+            <button className="ghost" onClick={handleSignUpClick}>Crea una Cuenta</button>
+          </div>
+        </div>
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default SignAndLog;
