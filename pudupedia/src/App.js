ffe946a50852389e0ddfbedb93a1d9953bb56fb7
Biank123 <biancadepetris1998@gmail.com>
2024-08-09 00:38:17 -0400

    burbles actualización v2

--- pudupedia/src/App.js
+++ pudupedia/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Burbles from './Components/MapPage/Burbles';
+// import Burbles from './Components/MapPage/Burbles';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import LandingPage from './Components/LandingPage/LandingPage';
 // import GamePage from './Components/GamePage/GamePage';
 // import TriviaGame from './Components/GamePage/TriviaGame/TriviaGame';
 // import NavBar from './Components/LandingPage/Navbar/NavBar';
-// import Mapita from './Components/MapPage/Mapita'
+
 
 function App() {
     return (
@@ -18,8 +18,8 @@ function App() {
         //         <Route path="/image/:id" element={<TriviaGame />} />
         //     </Routes>
         // </Router>
-        // <Mapita />
-       <Burbles />
+        
+    //    <Burbles />
     );
 }
 
--- pudupedia/src/Components/MapPage/Burbles.jsx
+++ pudupedia/src/Components/MapPage/Burbles.jsx
@@ -1,42 +1,43 @@
 import React, { useState } from 'react';
 import './Burbles.css';
 import Mapita from './Mapita';
-import imagePuduReading from '../GamePage/TriviaGame/PuduReading.jpg';
+import imagePuduReading from '../GamePage/TriviaGame/PuduReading.jpg'; // Imagen de ejemplo
 
 const Burbles = () => {
-  const [activeRegion, setActiveRegion] = useState(null);
+  const [activeMacrozone, setActiveMacrozone] = useState(null);
   const [bubbles, setBubbles] = useState([]);
   const [visibility, setVisibility] = useState(false);
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
-  const handleRegionClick = (region, regionBounds) => {
-    if (activeRegion === region) {
-      // Toggle visibility if the same region is clicked again
-      setVisibility(!visibility);
-    } else {
-      // Calculate new bubbles positions
-      const numBubbles = 5;
-      const angleStep = Math.PI / 4; // Adjust for the size of the arc
-      const radius = 100; // Adjust for the distance of the arc from the center of the region
-
-      const newBubbles = [];
-      for (let i = 0; i < numBubbles; i++) {
-        const angle = i * angleStep;
-        const x = regionBounds.x + regionBounds.width / 2 + radius * Math.cos(angle);
-        const y = regionBounds.y + regionBounds.height / 2 - radius * Math.sin(angle);
-
-        newBubbles.push({ x, y, image: imagePuduReading });
-      }
-
-      setBubbles(newBubbles);
-      setActiveRegion(region);
-      setVisibility(true);
+  const handleMacrozoneClick = (macrozone, macrozoneBounds) => {
+    if (!macrozoneBounds) {
+        console.error('macrozoneBounds is undefined:', macrozone);
+        return;
     }
-  };
+
+    if (activeMacrozone === macrozone) {
+        setVisibility(!visibility);
+    } else {
+        const numBubbles = 3;
+        const angleStep = (2 * Math.PI) / (numBubbles);
+        const radius = 30;
+
+        const newBubbles = [];
+        for (let i = 0; i < numBubbles; i++) {
+            const angle = i * angleStep;
+            const x = macrozoneBounds.x + (macrozoneBounds.width) + (radius * Math.cos(angle)*6) ;
+            const y = macrozoneBounds.y + (macrozoneBounds.height) - (radius * Math.sin(angle)*6) ;
+            newBubbles.push({ x, y, image: imagePuduReading });
+        }
+
+        setBubbles(newBubbles);
+        setActiveMacrozone(macrozone);
+        setVisibility(true);
+    }
+};
 
   const handleCloseBubbles = () => {
     setVisibility(false);
-    // Reset bubbles to trigger animation again
     setBubbles([]);
   };
 
@@ -50,7 +51,7 @@ const Burbles = () => {
 
   return (
     <div className="radial-menu">
-      <Mapita onRegionClick={handleRegionClick} />
+      <Mapita onMacrozoneClick={handleMacrozoneClick} />
       <div className={`larger-area ${visibility ? 'active' : ''}`}>
         {bubbles.map((bubble, index) => (
           <div
@@ -65,14 +66,13 @@ const Burbles = () => {
                 transform: hoveredIndex === index ? 'scale(1.2)' : 'scale(1)',
                 transition: 'transform 0.3s ease-in-out',
               }}
-              // no funciona que al hacer hover aumente de escala la imágen
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
             />
           </div>
         ))}
         {visibility && (
-          <button className="close-button" onClick={handleCloseBubbles}>Close</button>
+          <button className="close-button" onClick={handleCloseBubbles}>X</button>
         )}
       </div>
     </div>
