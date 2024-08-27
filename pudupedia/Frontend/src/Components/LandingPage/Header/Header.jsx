92ce99af3b885a837f0c98edd155e2e78b05e360
Biank123 <biancadepetris1998@gmail.com>
2024-08-27 11:36:08 -0400

    Update Header.jsx

--- pudupedia/Frontend/src/Components/LandingPage/Header/Header.jsx
+++ pudupedia/Frontend/src/Components/LandingPage/Header/Header.jsx
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import user_icon from './user_icon.png';
-import search_icon from './search_icon.png';
+import { Link } from 'react-router-dom';
 import share_arrow from './share_arrow.png';
 import pudupedia_logo_sized2 from './pudupedia_logo_sized2.png';
-import './Header.css'
+import './Header.css';
+import ShareMenu from './Share/ShareMenu';
 
 const Header = ({ className = '' }) => {
+  const [isShareMenuVisible, setShareMenuVisible] = useState(false);
+  const shareUrl = encodeURIComponent(window.location.href);
+
+  const toggleShareMenu = () => {
+    setShareMenuVisible(!isShareMenuVisible);
+  };
+
   return (
     <div className={`Header ${className}`}>
       <div className="headerContainer">
         <div className="logoContainer">
-          <img src={pudupedia_logo_sized2} alt='pudu_logo' className='puduLogo'></img>
+          <Link to="/">
+            <img src={pudupedia_logo_sized2} alt='pudu_logo' className='puduLogo'></img>
+          </Link>
         </div>
         <div className="miscContainer">
           <div className="buttonsWrapper">
-            <div className="shareButton">
+            <div className="shareButton" onClick={toggleShareMenu}>
+
               <img src={share_arrow} alt='share_icon' className='shareIcon'></img>
-            </div>
-            <div className="searchContainer">
-              <div className="searchButton">
-                <img src={search_icon} alt="search_icon" className="searchIcon" />
-                <input type="text" className="searchInput" placeholder="Buscar..." />
-              </div>
+              <ShareMenu url={shareUrl} isVisible={isShareMenuVisible} />
             </div>
           </div>
           <div className="languageToggle">
-            <p>Español | Inglés</p>
+
           </div>
           <div className="loginTab">
-          <a href="/signlog" target="_blank" rel="noopener noreferrer">
-            <p>L O G I N</p>
+            <a href="/signlog" target="_blank" rel="noopener noreferrer">
+              <p>L O G I N</p>
             </a>
             <a href="/signlog" target="_blank" rel="noopener noreferrer">
-            <img src={user_icon} alt="user_icon" className="userIcon"></img>
+              <img src={user_icon} alt="user_icon" className="userIcon"></img>
             </a>
           </div>
         </div>
